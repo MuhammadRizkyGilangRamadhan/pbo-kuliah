74eba2ac2b5570b8a10b7fd2f50f2c3b233c3a34
“MuhammadRizkyGIlangRamadhan” <rizkygithubb@gmail.com>
2023-12-14 10:21:25 +0800

    pbo kuliah.

---
+++ src/main/java/com/mycompany/kuliah/Kuliah.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kuliah;
+
+/**
+ *
+ * @author M Rizky Gilang R
+ */
+public class Kuliah {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
